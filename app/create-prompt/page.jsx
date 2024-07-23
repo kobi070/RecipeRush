2bbd1e4133d6de9202f2a933a7836170e59c1a6d
kobi070 <kobi070@gmail.com>
2024-07-23 11:05:40 +0300

    bug fix pag.jsx

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -13,7 +13,7 @@ const CreatePrompt = () => {
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
 
-  const createPrompt = async (e: FormEvent<HTMLFormElement>) => {
+  const createPrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
@@ -22,7 +22,7 @@ const CreatePrompt = () => {
         method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
-          userId: session?.user?.id,
+          userId: session?.user.id,
           tag: post.tag,
         }),
       });
